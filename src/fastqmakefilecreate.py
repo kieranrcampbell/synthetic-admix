dd3be8230dff0509a81097994a1e7848becd0513
Kieran Campbell <kieranrcampbell@gmail.com>
2015-01-29 11:45:45 +0000

    identify cells, download sra & convert to fastq.gz

--- src/fastqmakefilecreate.py
+++ src/fastqmakefilecreate.py
@@ -1,11 +1,18 @@
 """
 Create makefiles for the cluster for converting sra to fastq
+
+kieran.campbell@dpag.ox.ac.uk
 """
 
 import os
 
+"""
+Paths to data - edit to correct
+"""
 type1_dir = '/net/isi-scratch/kieran/admix/synthetic/synthetic-admix/data/type1'
 type2_dir =  '/net/isi-scratch/kieran/admix/synthetic/synthetic-admix/data/type2'
+makefile_dir = "/net/isi-scratch/kieran/admix/synthetic/synthetic-admix/data/makefiles"
+
 
 fastq_dump = "/net/isi-software/server/ncbi_sra_toolkit/ncbi_sra_toolkit_2.2.0/bin/fastq-dump" 
 opts = "--split-3 --gzip --outdir"
@@ -26,7 +33,7 @@ makefiles = makefiles + [makefile_template_2 + " " + d for d in [os.path.join(ty
 sra = type1_sra + type2_sra
 makefile_names = [s + "_makefile" for s in sra]
 
-makefile_dir = "/net/isi-scratch/kieran/admix/synthetic/synthetic-admix/data/makefiles"
+
 
 for i in range(len(makefile_names)):
 	pth = os.path.join(makefile_dir, makefile_names[i])
